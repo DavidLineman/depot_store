8971d2636b244f7774f2ee4bb62604050ad6559b
DavidLineman <dline215@gmail.com>
2024-06-07 14:04:12 -0400

    added cart functionality to combine line items

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -23,7 +23,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     follow_redirect!
 
     assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', 'Design and Build Great Web APIs'
+    assert_select 'li', '1 \u00D7 Design and Build Great Web APIs'
   end
 
   test "should show line_item" do
