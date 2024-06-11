70d66dba3bcad2c31bfe183641b6c4e58478cd9d
DavidLineman <dline215@gmail.com>
2024-06-11 14:33:37 -0400

    fixed line items test url error

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -54,7 +54,7 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to store_index_url_url, 
+      format.html { redirect_to store_index_url, 
         notice: "Your cart is currently empty" }
       format.json { head :no_content }
     end
