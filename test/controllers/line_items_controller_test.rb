105901d73550ee1c41c2a0b57743a82c04454763
DavidLineman <dline215@gmail.com>
2024-06-19 10:58:12 -0400

    added highlight feature to cart, fixed create line item test

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,13 +17,13 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create line_item" do
     assert_difference("LineItem.count") do
-      post line_items_url, params: { line_item: { product_id: @line_item.product_id } }
+      post line_items_url, params: { product_id: products(:ruby).id } 
     end
 
     follow_redirect!
 
     assert_select 'h2', 'Your Cart'
-    assert_select 'li', 'Programming Ruby 1.9'
+    assert_select 'td', 'Programming Ruby 1.9'
   end
 
   test "should show line_item" do
@@ -49,4 +49,14 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to line_items_url
   end
+
+  test 'should create line_item via turbo-stream' do 
+    assert_difference('LineItem.count') do 
+      post line_items_url, params: { product_id: products(:ruby).id },
+        as: :turbo_stream
+    end 
+
+    assert_response :success
+    assert_match /<tr class="line-item-highlight">/, @response.body
+  end 
 end
